0fed6e822b780a2dfb706ca81524baeac411786b
Manuel <njester5030@gmail.com>
2019-11-18 09:35:08 +0100

    calc examples added

---
+++ Tutorium/src/Termin3/Rechnen.java
@@ -0,0 +1,23 @@
+package Termin3;
+
+public class Rechnen {
+	public static void main(String[] args) {
+		int zahl = 5;
+		
+		zahl = zahl + 1;
+		zahl += 1;
+		zahl++;
+		++zahl;
+		
+		zahl = zahl + 5;
+		zahl += 5;
+		
+		
+		zahl = zahl * 5;
+		zahl *= 5;
+		
+		
+		zahl = zahl / 5;
+		zahl /= 5;
+	}
+}
