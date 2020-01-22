42d685a9df9a0c465b6325576d5add57827404ec
Manuel <njester5030@gmail.com>
2020-01-22 06:56:05 +0100

    Schachbrett hinzugefügt

---
+++ Tutorium/src/Termin9/Schachbrett.java
@@ -0,0 +1,33 @@
+package Termin9;
+
+public class Schachbrett {
+	boolean[][] brett = new boolean[8][8];
+	
+	public void setzeTurm(int x, int y) {
+		for (int row = 0; row < brett.length; row++) {
+			for (int col = 0; col < brett[row].length; col++) {
+				if (row == y || col == x) {
+					brett[row][col] = true;
+				}
+			}
+		}
+	}
+	
+	public void gibBedrohteFelderAus() {
+		for (int row = 0; row < brett.length; row++) {
+			for (int col = 0; col < brett[row].length; col++) {
+				System.out.print(brett[row][col] ? "X " : "O ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Schachbrett sb = new Schachbrett();
+		sb.gibBedrohteFelderAus();
+		sb.setzeTurm(3, 6);
+		sb.gibBedrohteFelderAus();
+	}
+	
+}
