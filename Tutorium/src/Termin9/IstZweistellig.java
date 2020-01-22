6897c95ae7be282ef37e895a2a818cc0e9075d2c
Manuel <njester5030@gmail.com>
2020-01-22 09:42:37 +0100

    negative Beachtung eingebaut

--- Tutorium/src/Termin9/IstZweistellig.java
+++ Tutorium/src/Termin9/IstZweistellig.java
@@ -6,15 +6,19 @@ public class IstZweistellig {
 		System.out.println("1: " + istZweistellig(1));
 		System.out.println("9: " + istZweistellig(9));
 		System.out.println("10: " + istZweistellig(10));
+		System.out.println("-9: " + istZweistellig(-9));
+		System.out.println("-10: " + istZweistellig(-10));
 		System.out.println("50: " + istZweistellig(50));
 		System.out.println("99: " + istZweistellig(99));
 		System.out.println("100: " + istZweistellig(100));
+		System.out.println("-99: " + istZweistellig(-99));
+		System.out.println("-100: " + istZweistellig(-100));
 		System.out.println("1000: " + istZweistellig(1000));
 
 	}
 	
 	public static boolean istZweistellig(int arg) {
-		return arg > 9 && arg < 100;
+		return (arg > 9 && arg < 100) || (arg < -9 && arg > -100);
 	}
 
 }
